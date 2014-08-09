63fbbfb3697d424e013985c87fe1c00c6eeb4b11
Markusli <markus.lippus@gmail.com>
2014-08-09 19:07:05 +0300

    fixed 'sketchy' read finder and added two output files into which the reads are sorted according to whether they are 'sketchy' or not.

--- find_next_nucleotide_3prim.py
+++ find_next_nucleotide_3prim.py
@@ -2,22 +2,25 @@ import pysam
 from Bio import SeqIO
 for seq_record in SeqIO.parse("refgenome.fasta", "fasta"):
     sequence=seq_record.seq
-    
+samfile = pysam.Samfile("aligned.sam", "r" )
+
+output_correct=pysam.Samfile('aligned_right.sam', 'w', template = samfile)
+output_sketchy=pysam.Samfile('aligned_sketchy.sam', 'w', template = samfile)
 
 n = 0
 for read in samfile:
     if read.is_reverse == True:
         check_pos = read.pos - 1
-        if sequence[check_pos] == 'A':
-            print 'fuckered'
+        if sequence[check_pos] == 'T':
+            output_sketchy.write(read)
         else:
-            print 'unfuckered'
+            output_correct.write(read)
     else:
         check_pos = read.pos + read.qlen
-        if sequence[check_pos] == 'A':
-            print 'fuckered'
+        if sequence[check_pos] == 'T':
+            output_sketchy.write(read)
         else:
-            print 'unfuckered'
+            output_correct.write(read)
         
 
     n += 1
