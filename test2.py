ccc55afc5c75207a25ddc65ab7c290b0118dae4f
David Schryer <schryer@gmail.com>
2014-08-11 15:38:04 +0300

    Changed test2.py to plot to file.

--- test2.py
+++ test2.py
@@ -2,7 +2,6 @@ from pybedtools import BedTool
 from collections import Counter
 import matplotlib
 import matplotlib.pyplot as plt
-import pylab as pl
 import numpy as np
 import csv
 
@@ -10,7 +9,8 @@ import csv
 print """This script will take a .bed file as input, find the starting points of all reads 
 and output a table containing all thestarting points and the number of reads that start at this point"""
 
-filepath = raw_input("Enter the path of the file you wish to analyze: ")
+#filepath = raw_input("Enter the path of the file you wish to analyze: ")
+filepath = 'test2.bed'
 tool1 = BedTool(filepath)
 
 
@@ -19,30 +19,49 @@ tool1 = BedTool(filepath)
 # Counts the occurrences of every start position and stores the 
 # position and the number of occurrences in a dictionary called "result"
 
-storage = []
+def get_position_counts(bed_file, direction):
 
-for feature in tool1:
+    positions = []
+    
+    for feature in bed_file:
 
-    if feature.strand == '+':
-        start_pos = (feature.start)
-        storage.append(start_pos)
-    elif feature.strand == '-':
-        start_pos = (feature.stop - 1)
-        storage.append(start_pos)
+        if direction != feature.strand:
+            continue
+        
+        if direction == '+':
+            start_pos = (feature.start)
+        elif direction == '-':
+            start_pos = (feature.stop - 1)
 
-result = Counter(storage)
+        positions.append(start_pos)
+
+    return Counter(positions)
+
+pc = get_position_counts(tool1, '+')
+
+x = []
+y = []
+ptcnt = 0
+for index in range(min(pc), max(pc)):
+    if ptcnt > 20:
+        continue
+        
+    if index in pc:
+        ptcnt += 1
+        y.append(pc[index])
+    else:
+        y.append(0)
+    x.append(index)    
 
 # Draws a histogram of the data from the file. So far haven't figured out how to add labels to every starting point.
 
-length = len(storage)
-fig = plt.figure(figsize=(80,40), dpi=100)   
-pl.hist(storage, length, histtype = "step", align = "mid", color = "black", label = "reads")
-plt.legend()
-plt.show()
+fig = plt.figure(figsize=(8,6))   
+plt.scatter(x, y)#, histtype = "step", align = "mid", color = "black", label = "reads")
+plt.savefig('test2.pdf')
 
 # Outputs a table with the starting points and the number of its occurrences.
 output = csv.writer(open("output.csv", "w"))
-for key, val in result.items():
+for key, val in pc.items():
     output.writerow([key, val])
 print "Analysis complete"
 
